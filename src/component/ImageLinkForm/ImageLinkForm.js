c7864155f45813088bfd25652bf0b3786eba2f8e
gadfrey <gadfreybalacy@gmail.com>
2019-09-08 18:55:53 -0700

    fix sub title

--- src/component/ImageLinkForm/ImageLinkForm.js
+++ src/component/ImageLinkForm/ImageLinkForm.js
@@ -3,7 +3,7 @@ import './ImageLinkForm.css'
 const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return(
         <div>
-            <p className="center f3">{'This Magin Brain will detect faces in you pictures. Give it a try!!!'}</p>
+            <p className="center f3">{'Detect Faces. Give It A Try!!!'}</p>
             <div className="center">
                 <div className="pa4 br3 shadow-5 form center">
                     <input className="f4 pa2 w-70 center" type="text" onChange={onInputChange}/>
